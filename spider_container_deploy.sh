99decdfb510981094cf2f6750093c6ead95b013a
Meiert Willem Grootes <42940327+meiertgrootes@users.noreply.github.com>
2024-03-30 00:38:47 +0100

    Update spider_container_deploy.sh
    remove commented out clone of JupyterDaskOnSlurm
    
    Co-authored-by: Ou Ku <o.ku@esciencecenter.nl>

--- spider_container_deploy.sh
+++ spider_container_deploy.sh
@@ -9,7 +9,6 @@ fi
 #clone the hpc_container_wrapper JupyterDaskOnSLurm repositories
 #tis assumes that git access (ssh key) has been configured on spider by the user
 git clone git@github.com:CSCfi/hpc-container-wrapper.git
-#git clone git@github.com:RS-DAT/JupyterDaskOnSLURM.git
 
 #change directory to the hpc-container-wrapper dir
 cd hpc-container-wrapper
